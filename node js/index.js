fe00d4b0bd5d9c6b8f4b03c27ec513a1d43746db
ShishiSkrub <97239751+ShishiSkrub@users.noreply.github.com>
2024-04-22 12:07:01 +0200

    Add files via upload

---
+++ node js/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const routes = require("./routes/start");
+const app = express();
+const port = 3000;
+const cors = require("cors");
+
+app.use(express.json());
+app.use("/", routes);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
